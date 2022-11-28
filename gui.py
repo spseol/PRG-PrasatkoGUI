f0c2fdd994d9f52aa8f83fe29488dec09f4fa08d
Marrek Nožka <marek@tlapicka.net>
2022-11-28 10:30:45 +0100

    image

--- gui.py
+++ gui.py
@@ -2,23 +2,38 @@
 
 from os.path import basename, splitext
 import tkinter as tk
+from PIL import Image, ImageTk
 from prasatko import Prasatko
 
 # from tkinter import ttk
 
 
 class Application(tk.Tk):
-    name = basename(splitext(basename(__file__.capitalize()))[0])
-    name = "Foo"
+    name = "Prasátko"
 
     def __init__(self):
         super().__init__(className=self.name)
         self.title(self.name)
         self.bind("<Escape>", self.quit)
-        self.lbl = tk.Label(self, text="Hello World")
+        self.lbl = tk.Label(self, text="Prasátko")
         self.lbl.pack()
-        self.btn = tk.Button(self, text="Quit", command=self.quit)
-        self.btn.pack()
+        imaghandler = Image.open("img/prasatko.png")
+        tkimage = ImageTk.PhotoImage(imaghandler)
+        self.img = tk.Label(self, image=tkimage)
+        self.img.image = tkimage
+        self.img.pack()
+        self.entry = tk.Entry(self)
+        self.entry.insert(0, 1234)
+        self.entry.pack()
+
+        text = """1000 x 1
+200 x 1
+20 x 1
+10 x 1
+2 x 2
+"""
+        self.message = tk.Message(self, text=text)
+        self.message.pack()
 
     def quit(self, event=None):
         super().quit()
